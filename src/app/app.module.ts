5ceeeb535c469138fe5b96dee9c4013a25f81fdb
yogananda222 <yoganandanaidu222@gmail.com>
2025-01-28 06:31:28 +0530

    cinebloom

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';  // Import HttpClientModules
 import { HomeComponent } from './pages/home/home.component';  // Import HomeComponent
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,  // Ensure HttpClientModule is imported
-    HomeComponent,      // Import HomeComponent here (no need to declare it)
-  ],
-  providers: [],
-  bootstrap: [],  // Bootstrap AppComponent here
+  declarations: [],
+  imports: [BrowserModule],
+  providers: [provideHttpClient()],
+  bootstrap: [],
 })
 export class AppModule { }
